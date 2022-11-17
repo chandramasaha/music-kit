d3f46b10976304d330b58dd4011889199c03946e
Chandrama Saha <chandramasaha@gmail.com>
2022-11-18 03:18:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Music Kit
+ A collection of musical instruments, that play their respective sound virtually with the help of JavaScript.
---
+++ script.js
@@ -0,0 +1,16 @@
+const elements = ["synth", "piano", "guitar", "trumpet", "drum", "flute"];
+for (let i = 0; i < elements.length; i++) {
+    {
+        const element = document.getElementById(elements[i]);
+        element.addEventListener("click", clicked);
+    }
+}
+
+function clicked(event) {
+    let element = event.target;
+    let id = element.id;
+    let path = 'sounds/' + id + '.wav';
+
+    const sound = new Audio(path);
+    sound.play();
+}
